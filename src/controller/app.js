7c64ebefc2b618a12f63ff5c2cb33a32a647e864
vicky <vivi.melo2511@gmail.com>
2024-11-07 15:22:49 -0300

    adicionei mais itens

--- src/controller/app.js
+++ src/controller/app.js
@@ -1,9 +1,17 @@
-import * as db from "../repository/user.js";
+import * as db from "../repository/app.js";
 import { autenticar } from "../auth/jwt.js";
  
 import { Router } from "express";
 const endpoints = Router();
 
+endpoints.post('/criar', autenticar, async (req, resp) => {
+    let user = req.body;
+    let id = await db.criaruser(user);
 
+    resp.send({
+        idUser: id
+    })
+})
 
 export default endpoints;
+
